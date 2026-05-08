4b72f7e425744be058bb4fa6e7c136a965472c86
vnxidn-droid <vnxidn@gmail.com>
2026-05-09 00:47:46 +0700

    pp

--- README.md
+++ README.md
@@ -9,8 +9,16 @@
 <img src="https://github.com/vnxidn-droid/tugaspro/blob/948a2dbea390fe02da130a26a97ea2ee99f526c8/assets/images/me.jpg" width="80" alt="WebToApp Logo"/>
 
 
-deskripsi apps
-pending dulu ok ok 
+deskripsi:
+rencananya apps ini bisa dapat mengelola file dll sekian untuk rencana kali ini.
+
+
+
+
+
+
+
+
 di dukung oleh:
 
 <img src="https://github.com/vnxidn-droid/assetsgithub/blob/a013f7a637bc3570c52496bb28606269ad10d062/images/BIOHUMAN.png" width="160" alt="dukungan"/>
--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,118 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:permission_handler/permission_handler.dart';
+import 'package:file_picker/file_picker.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    home: new Ui1finn(
-
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: VynTeam(),
     ),
-  ));
+
+  );
 }
 
 
-class Ui1finn extends StatelessWidget{
+class VynTeam extends StatefulWidget{
   @override
-  Widget build(BuildContext context){
+  State<VynTeam> createState() => _VynTeamstate();
+}
+
+class _VynTeamstate extends State<VynTeam> {
+  String status = "Checking permission...";
+  @override
+  void initState() {
+    super.initState();
+    requestPermission();
+  }
+  Future<void> requestPermission() async {
+    PermissionStatus permission;
+    permission = await Permission.manageExternalStorage.request();
+    if (permission.isGranted){
+      setState(() {
+        status = """[status info]
+    ok boskuhh makasih udah kasih akses penyimpanan""";
+
+      });
+    }
+    else if(permission.isDenied){
+      setState(() {
+        status = """[status info]
+    akses penyimpanan di tolak
+    silahkan buka pengaturan""";
+ });
+      openAppSettings();
+    }
+
+
+
+
+  }
+  Widget build(BuildContext context) {
     return Scaffold(
-    body:
-        new Center(
-          child: Column(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
 
-             mainAxisAlignment: MainAxisAlignment.center,
-              children: [
+        onPressed: () async {
+          await Permission.manageExternalStorage.request();
+        },
 
-                Text(
-                  "welcome to",
-                  style: TextStyle(
-                    fontFamily: "cyber2",
-                    fontSize: 33,
-                    color: Colors.blue
-                  ),
-                ),
-
-                SizedBox( height: 60,),
-
-                Text(
-                  "mepro",
-                  style: TextStyle(
-                    fontFamily: "cyber2",
-                    fontSize: 88,
-                  ),
-                ),
+        child: const Icon(Icons.settings),
+      ),
 
 
+      backgroundColor: Colors.black,
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Text(status,
+            style: TextStyle(
+              color: Colors.red,
+              fontFamily: "cyber2",
+              fontSize: 12,
+            ),),
+
+            Text("welcome to",
+            style: TextStyle(
+              fontFamily: "cyber2",
+              fontSize: 33,
+              color: Colors.green,
+              height: 1,
+
+            ),),
+
+            Text("ME PRO",
+              style: TextStyle(
+                fontFamily: "cyber2",
+                fontSize: 100,
+                color: Colors.green,
+                height: 1,
+              ),
+
+            ),
+            Text(
+              """aplikasi ini di buat oleh
+              mahasiswa universitas islam madura""",
+              style: TextStyle(
+                fontFamily: "cyber2",
+                fontSize: 12,
+                color: Colors.green,
 
 
 
-                ElevatedButton(
-                  onPressed: () {
-                    print("test");
-                  },
 
-                  child: Text("test"),
+            ),
+            ),
 
-
-                ),
-
-
-              ],
-
-          ),
-
-
-
-
-
-          ),
-
+          ],
+        ),
+      ),
 
     );
   }
+
 
 }
